96eff1a495b3c87aeb7d0c165e1d48c1d33c84d4
Michael Medina <mikematics22800@gmail.com>
2025-02-03 18:49:50 -0500

    updated UI

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -18,17 +18,20 @@ function Home() {
     const date3HrsLater = new Date((period.dt + 10800) * 1000)
     return (
       <div className="period" key={i}>
-        <h2>{getTime(date)} - {getTime(date3HrsLater)}</h2>
-        <div className="flex items-center">
-          <img src={icon(period.weather[0].icon, '')}/>
-          <h2>{period.weather[0].description.split(' ').map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')}</h2>
+        <div className="flex items-center justify-between">
+         <h2>{getTime(date)} - {getTime(date3HrsLater)}</h2>
+          <img className="w-16 h-16" src={icon(period.weather[0].icon, '@4x')}/>
         </div>
         <div className="flex flex-col gap-1">
-          <h2>Temperature: {kToF(period.main.temp)}°F</h2>
-          <h2>Wind: {degToDir(period.wind.deg)} at {Math.round(period.wind.speed)} mph</h2>
-          <h2>Humidity: {period.main.humidity}%</h2>
-          <h2>Heat Index: {kToF(period.main.feels_like)}°F</h2>
-          <h2>Air Pressure: {period.main.pressure} mb</h2>
+          <h2>{period.weather[0].description.split(' ').map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')}</h2>
+          <div className="flex justify-between">
+            <h2>High/Low</h2>
+            <h2>{kToF(period.main.temp_max)}/{kToF(period.main.temp_min)}°F</h2>
+          </div>
+          <div className="flex justify-between">
+            <h2>Humidity</h2>
+            <h2>{period.main.humidity}%</h2>
+          </div>
         </div>
       </div>
     )
@@ -40,22 +43,24 @@ function Home() {
       {current && forecast ? (
         <div id="weather">
           <div id="current">
-            <h1>Currently at {current.name}, {current.sys.country}</h1>
-            <div className="flex items-center">
-              <img src={icon(current.weather[0].icon, '@2x')}/>
-              <h2>{current.weather[0].description.split(' ').map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')}</h2>
+            <div className="flex items-center justify-between">
+              <h1>Currently at {current.name}, {current.sys.country}</h1>
+              <img className="h-40 w-40" src={icon(current.weather[0].icon, '@4x')}/>
             </div>
-            <div className="flex flex-wrap sm:gap-4 gap-2">
-              <h2>Temperature: {kToF(current.main.temp)}°F</h2>
-              <h2>Wind: {degToDir(current.wind.deg)} at {Math.round(current.wind.speed)} mph</h2>
-              <h2>Humidity: {current.main.humidity}%</h2>
-              <h2>Heat Index: {kToF(current.main.feels_like)}°F</h2>
-              <h2>Air Pressure: {current.main.pressure} mb</h2>
+            <div className="flex flex-wrap sm:gap-4 gap-2 w-full justify-between items-center">
+              <h2>{current.weather[0].description.split(' ').map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')}</h2>
+              <h2>Temperature {kToF(current.main.temp)}°F</h2>
+              <h2>Wind {degToDir(current.wind.deg)} at {Math.round(current.wind.speed)} mph</h2>
+              <h2>Humidity {current.main.humidity}%</h2>
+              <h2>Heat Index {kToF(current.main.feels_like)}°F</h2>
+              <h2>Air Pressure {current.main.pressure} mb</h2>
             </div>
           </div>
           <div id="forecast">
             <h1>Five Day Forecast for {forecast.city.name}, {forecast.city.country}</h1>
-            <TempChart/>
+            <div className="w-full bg-[skyblue] p-5 rounded-lg">
+              <TempChart/>
+            </div>
             <div id="periods">
               {periods}
             </div>
@@ -63,7 +68,7 @@ function Home() {
         </div>
       ) : (
         <div id="loader">
-          <CircularProgress size={"10rem"}/>
+          <CircularProgress size={"5rem"}/>
         </div>
       )}
     </div>
