844a6defb53d9950e86d39204a38ae208c3ff55d
Michael Medina <mikematics22800@gmail.com>
2026-04-20 11:19:14 -0400

    ui tweak

--- src/components/ForecastPeriods.jsx
+++ src/components/ForecastPeriods.jsx
@@ -5,7 +5,11 @@ import { Context } from "./WeatherContext"
 const ForecastPeriods = () => {
   const { forecast } = useContext(Context)
   const list = forecast?.list ?? []
-  return list.map((period, i) => <ForecastPeriod key={i} period={period} />)
+  return list.map((period, i) => (
+    <div key={i} className="forecast-period-item">
+      <ForecastPeriod period={period} />
+    </div>
+  ))
 }
 
 export default ForecastPeriods
